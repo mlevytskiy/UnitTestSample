8d76a38ee2747be67da0cdd665e2c3b94ee8a567
Michael Pustovit <mpustovit@stanfy.com.ua>
2015-03-27 21:22:40 +0200

    Add implementations for RandomActivity and RandomActivityTest

--- src/main/java/org/unittestsample/RandomActivity.java
+++ src/main/java/org/unittestsample/RandomActivity.java
@@ -2,6 +2,19 @@ package org.unittestsample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.unittestsample.data.RandomRequest;
+import org.unittestsample.data.RandomResponse;
+
+import java.util.Arrays;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+import static android.view.View.*;
 
 /**
  *  Activity with field with random numbers and refresh button.
@@ -10,10 +23,68 @@ import android.os.Bundle;
  */
 public class RandomActivity extends Activity {
 
+  private Callback<RandomResponse> randomResponseCallback = new Callback<RandomResponse>() {
+    @Override
+    public void success(RandomResponse randomResponse, Response response) {
+      processSuccess(randomResponse);
+    }
+
+    @Override
+    public void failure(RetrofitError error) {
+      processError(error);
+    }
+  };
+
+  private OnClickListener refreshBtnClickListener = new OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      startLoadRandomNumbers();
+    }
+  };
+  private View refreshBtn;
+  private View loadingView;
+  private TextView randomView;
+
+  void startLoadRandomNumbers() {
+    loadingView.setVisibility(VISIBLE);
+    randomView.setVisibility(GONE);
+    refreshBtn.setVisibility(GONE);
+
+    final RandomApi api = ((UnitTestApplication) getApplication()).getApi();
+    api.getRandoms(new RandomRequest(3, 1, 100), randomResponseCallback);
+  }
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_random);
+
+    loadingView = findViewById(R.id.random_loading);
+    randomView = (TextView) findViewById(R.id.random_text);
+
+    refreshBtn = findViewById(R.id.random_refresh);
+    refreshBtn.setOnClickListener(refreshBtnClickListener);
+
+    // Load random numbers on start.
+    startLoadRandomNumbers();
+  }
+
+  void makeMainViewsVisible(final String text) {
+    loadingView.setVisibility(GONE);
+
+    randomView.setText(text);
+    randomView.setVisibility(VISIBLE);
+
+    refreshBtn.setVisibility(VISIBLE);
+  }
+
+  void processSuccess(final RandomResponse response) {
+    makeMainViewsVisible(Arrays.toString(response.getData()));
+  }
+
+  void processError(final RetrofitError error) {
+    makeMainViewsVisible("Error");
   }
 
 }
--- src/test/java/org/unittestsample/RandomActivityTest.java
+++ src/test/java/org/unittestsample/RandomActivityTest.java
@@ -1,11 +1,103 @@
 package org.unittestsample;
 
+import android.view.View;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.unittestsample.data.RandomRequest;
+import org.unittestsample.data.RandomResponse;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+
+import static org.assertj.android.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+// Robolectric supports only SDK 18- so we need emulate it.
+// Also now gradle and Android Studio JUnit test runner run tests from different working
+// directories so we need to specify Android manifest location implicitly.
+// WARNING: if your application uses flavors then implicit manifest location could break tests.
+@Config(emulateSdk = 18, manifest = "src/main/AndroidManifest.xml")
+
+// We have to use custom Robolectric test runner because with it Robolectric can control
+// class loading.
+@RunWith(RobolectricTestRunner.class)
 public class RandomActivityTest {
 
-  // TODO 1. Make setup method which creates activity.
-  // TODO 2. Test initial state
-  // TODO 3. Test success result processing
-  // TODO 4. Test error processing
-  // TODO 5. Test refresh
+  private RandomActivity activity;
+  private View loadbar;
+  private View refreshBtn;
+  private TextView randomView;
+  
+  @Before
+  public void setUp() throws Exception {
+    activity = Robolectric.setupActivity(RandomActivity.class);
 
+    loadbar = activity.findViewById(R.id.random_loading);
+    refreshBtn = activity.findViewById(R.id.random_refresh);
+    randomView = (TextView) activity.findViewById(R.id.random_text);
+  }
+
+  @Test
+  public void shouldShowOnlyLoadbarInitially() throws Exception {
+    assertThat(loadbar).isVisible();
+    assertThat(refreshBtn).isGone();
+    assertThat(randomView).isGone();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void shouldInitRundomNumbersLoadingOnStart() throws Exception {
+    final RandomApi api = ((UnitTestApplication) Robolectric.application).getApi();
+    verify(api).getRandoms(any(RandomRequest.class), any(Callback.class));
+  }
+
+  @Test
+  public void shouldShowLoadingResult() throws Exception {
+    activity.processSuccess(new RandomResponse(new int[]{1, 2, 3}));
+    assertThat(loadbar).isGone();
+
+    assertThat(randomView).isVisible();
+    assertThat(randomView).containsText("[1, 2, 3]");
+
+    assertThat(refreshBtn).isVisible();
+  }
+
+  @Test
+  public void shouldProcessError() throws Exception {
+    activity.processError(mock(RetrofitError.class));
+    assertThat(loadbar).isGone();
+
+    assertThat(randomView).isVisible();
+    assertThat(randomView).containsText("Error");
+
+    assertThat(refreshBtn).isVisible();
+  }
+
+  @Test
+  public void shouldShowLoadingOnRefresh() throws Exception {
+    activity.processSuccess(new RandomResponse(new int[]{1, 2, 3}));
+    Robolectric.clickOn(refreshBtn);
+
+    assertThat(loadbar).isVisible();
+    assertThat(randomView).isGone();
+    assertThat(refreshBtn).isGone();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void shouldLoadNewDataOnRefresh() throws Exception {
+    activity.processSuccess(new RandomResponse(new int[] {1, 2, 3}));
+    Robolectric.clickOn(refreshBtn);
+
+    final RandomApi api = ((UnitTestApplication) Robolectric.application).getApi();
+    //This method should be called twice: on onCreate and on click on "Refresh" button
+    verify(api, times(2)).getRandoms(any(RandomRequest.class), any(Callback.class));
+  }
 }
